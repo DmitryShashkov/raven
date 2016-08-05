a383d627c1bca91cbb5225fb225197195f70629f
DmitryShashkov <dmitry.the.wise@gmail.com>
2016-08-05 23:02:11 +0300

    Adjustments for Heroku

--- public/js/app.js
+++ public/js/app.js
@@ -35,7 +35,7 @@ var App = React.createClass({
         var state = {
             userName: chance.capitalize(chance.word()) + ', ' + chance.suffix(),
             route: window.location.hash.substr(1),
-            socket: new WebSocket('wss://localhost:1981'),
+            socket: new WebSocket('wss://quote-the-raven.herokuapp.com:1981'),
             userID: '',
             gamesList: []
         };
